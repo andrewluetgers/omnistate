795c179e0c4beb94371c790b52f3c4dcd2783a62
andrew luetgers <andrew.luetgers@gmail.com>
2015-12-04 05:21:59 -0600

    removed some cruft, started todomvc example

---
+++ examples/todomvc/app/operations/operations.js
@@ -0,0 +1,11 @@
+
+
+module.exports = {
+
+	any: {
+		operation: function(n, d) {
+			console.log("state changed", arguments);
+		}
+	}
+
+};
--- examples/todomvc/webpack.config.js
+++ examples/todomvc/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	devtool: 'cheap-module-eval-source-map',
 	entry: [
 		'webpack-hot-middleware/client',
-		'./index'
+		'./app/App.jsx'
 	],
 	output: {
 		path: path.join(__dirname, 'dist'),
@@ -18,33 +18,23 @@ module.exports = {
 		new webpack.NoErrorsPlugin()
 	],
 	module: {
-		loaders: [{
-			test: /\.js$/,
-			loaders: [ 'babel' ],
-			exclude: /node_modules/,
-			include: __dirname
-		}, {
-			test: /\.css?$/,
-			loaders: [ 'style', 'raw' ],
-			include: __dirname
-		}]
+		loaders: [
+			{test: /\.js$|\.jsx$/, loaders: ['babel'], exclude: /node_modules/},
+			{test: /\.styl$|\.css$/, loaders: ["style", "css", "stylus"]}
+		]
 	}
 };
 
 
 // When inside omnistate repo, prefer src to compiled version.
 // You can safely delete these lines in your project.
-var fs = require('fs'),
-	omnistateSrc = path.join(__dirname, '..', '..', 'lib');
-
-if (fs.existsSync(omnistateSrc)) {
-	// Resolve omnistate to source
-	module.exports.resolve = { alias: { 'omnistate': omnistateSrc } };
-	// Compile omnistate from source
-	module.exports.module.loaders.push({
-		test: /\.js$/,
-		loaders: [ 'babel' ],
-		include: omnistateSrc
-	})
-}
+//var fs = require('fs'),
+//	omnistateSrc = path.join(__dirname, '..', '..', 'lib');
+//
+//if (fs.existsSync(omnistateSrc)) {
+//	// Resolve omnistate to source
+//	module.exports.resolve = { alias: { 'omnistate': omnistateSrc } };
+//	// Compile omnistate from source
+//	module.exports.module.loaders.push({test: /\.js$|\.jsx$/, loaders: [ 'babel' ], include: omnistateSrc})
+//}
 
